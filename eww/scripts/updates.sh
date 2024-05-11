6dbc1e253c82d8a9907a6764fba335a5e976d237
Sergio Ricaflor <sergioricaflor@gmail.com>
2024-05-11 19:43:18 -0400

    asdasdasd

--- eww/scripts/updates.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Este script cuenta el número de actualizaciones disponibles y elimina espacios vacíos
-conteo=$(dnf check-update --quiet | grep '^[A-Za-z0-9]' | awk 'NF' | wc -l)
-
-echo "󰏔 $conteo"
